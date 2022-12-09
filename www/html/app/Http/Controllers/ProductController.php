d50462cdfda05270bd7ee170492fdcc743e035cf
Igor <elepsis@bk.ru>
2022-12-09 13:11:33 +0300

    cleaning...

--- www/html/app/Http/Controllers/ProductController.php
+++ www/html/app/Http/Controllers/ProductController.php
@@ -11,7 +11,6 @@ class ProductController extends Controller
     public function show($groupId, $productId) {
         $menu = new Menu($groupId);
         $breadMenu = $menu->breadMenu;
-        $bread = $menu->bread;
 
         $product = Product::getById($productId);
         return view('app/product', compact('product', 'breadMenu'));
